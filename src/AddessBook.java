88aeabce6a32102db62e7005314791805c393a13
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-09-25 06:19:05 -0400

    created a main method with a print message

---
+++ src/AddessBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AddessBook {
+    ArrayList<BuddyInfo> buddyInfos;
+
+    public AddessBook(){
+        buddyInfos = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(){
+
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main (String[] args) {
+        System.out.println("Address Book");
+    }
+}
