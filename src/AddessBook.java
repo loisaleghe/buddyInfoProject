3c0a179744ce71cb812a3012ab9ff3b02a48743c
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-09-25 06:28:58 -0400

    Added remove and add buddy method in main

--- src/AddessBook.java
+++ src/AddessBook.java
@@ -7,15 +7,19 @@ public class AddessBook {
         buddyInfos = new ArrayList<BuddyInfo>();
     }
 
-    public void addBuddy(){
-
+    public void addBuddy(BuddyInfo aBuddy){
+        buddyInfos.add(aBuddy);
     }
 
-    public void removeBuddy(){
-
+    public void removeBuddy(BuddyInfo aBuddy){
+    buddyInfos.remove(aBuddy);
     }
 
     public static void main (String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddessBook addressBook = new AddessBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
